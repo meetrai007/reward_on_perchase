f8952efe714e89530b8b139da1d10ebd7c3fd886
meetrai007 <meetrai101@gmail.com>
2025-09-17 09:38:21 +0530

    added facicon for this project

--- reward_on_perchase/settings.py
+++ reward_on_perchase/settings.py
@@ -126,7 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Where Django looks for static files during development
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # your global static folder
+]
+
+# Where collectstatic will copy files in production
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
